75dd1946d7e4c62f1621725fb5996d12ab68e10f
joeprincer <liujiazhou@gmail.com>
2017-10-25 15:34:52 +1100

    checkpoint1
    checkpoint1

---
+++ E-Plant/E-Plant/newKnowledge.swift
@@ -0,0 +1,29 @@
+//
+//  newKnowledge.swift
+//  E-Plant
+//
+//  Created by 郁雨润 on 25/10/17.
+//  Copyright © 2017 Monash University. All rights reserved.
+//
+
+import UIKit
+
+class newKnowledge: NSObject {
+    var title:String?
+    var category:String?
+    var article:String?
+    
+    override init(){
+        title = "Undefined"
+        category = "Undefined"
+        article = "Undefined"
+    }
+    
+    init(title:String, category:String, article:String){
+        self.title = title
+        self.article = article
+        self.category = category
+    }
+    
+
+}
