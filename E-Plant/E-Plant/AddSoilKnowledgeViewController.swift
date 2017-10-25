2aa8836fd513a414096d24c8c7a07c0da14dbea8
joeprincer <liujiazhou@gmail.com>
2017-10-25 16:02:26 +1100

    checkpoint2
    checkpoint2

--- E-Plant/E-Plant/AddSoilKnowledgeViewController.swift
+++ E-Plant/E-Plant/AddSoilKnowledgeViewController.swift
@@ -7,11 +7,25 @@
 //
 
 import UIKit
+import CoreData
 
-class AddSoilKnowledgeViewController: UIViewController {
+protocol addKnowledgeDelegate {
+    func addKnowledge(knowledge: newKnowledge)
+    }
+
+class AddSoilKnowledgeViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var titleField: UITextField!
+    @IBOutlet weak var articleField: UITextView!
+    
+    var knowlege: KnowledgeBase!
+    var managedContext: NSManagedObjectContext?
+    var appDelegate: AppDelegate?
+    var delegate: addKnowledgeDelegate?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        titleField.delegate = self
 
         // Do any additional setup after loading the view.
     }
@@ -20,16 +34,63 @@ class AddSoilKnowledgeViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(false)
     }
-    */
+    
+    // MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard when return key is pressed
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func getContext () -> NSManagedObjectContext {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        return appDelegate.persistentContainer.viewContext
+    }
+    
+    @IBAction func saveButton(_ sender: Any) {
+         let isPresentingInAddMode = presentingViewController is UITabBarController
+         if isPresentingInAddMode {
+            if (titleField.text!.trimmingCharacters(in: .whitespaces).isEmpty){  // the name cannot be empty
+                showAlert(title: "title")
+            }
+            else {
+                let newTitle = titleField.text
+                let newArticle = articleField.text
+                let newItem = newKnowledge(title: newTitle!, category: "soil Knowledge", article: newArticle!)
+                self.delegate!.addKnowledge(knowledge: newItem)
+                dismiss(animated: true, completion: nil)
+            }
+        }
+    }
+    
+    @IBAction func cancelButton(_ sender: Any) {
+        // Dismiss the view controller depending on the context it was presented
+        let isPresentingInAddMode = presentingViewController is UITabBarController
+        if isPresentingInAddMode {
+            dismiss(animated: true, completion: nil)
+        } else {
+            
+            navigationController!.popViewController(animated: true)
+        }
+    }
+    
+    
+    
+    
+    
+    
+    // alert function
+    func showAlert(title: String){
+        let alertVC = UIAlertController(title: "Warning", message: "\(title) can not be empty", preferredStyle: UIAlertControllerStyle.alert)
+        let acSure = UIAlertAction(title: "OK", style: UIAlertActionStyle.destructive) { (UIAlertAction) -> Void in
+            print("click Sure")
+        }
+        alertVC.addAction(acSure)
+        self.present(alertVC, animated: true, completion: nil)
+    }
 
+    
 }
