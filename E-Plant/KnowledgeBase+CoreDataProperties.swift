bbc1af4ea0bbbb4d3cfa54d46455e92ae5b7abf7
joeprincer <liujiazhou@gmail.com>
2017-10-25 15:25:42 +1100

    CoreDataStart
    Database builded

---
+++ E-Plant/KnowledgeBase+CoreDataProperties.swift
@@ -0,0 +1,23 @@
+//
+//  KnowledgeBase+CoreDataProperties.swift
+//  E-Plant
+//
+//  Created by 郁雨润 on 25/10/17.
+//  Copyright © 2017 Monash University. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+extension KnowledgeBase {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<KnowledgeBase> {
+        return NSFetchRequest<KnowledgeBase>(entityName: "KnowledgeBase")
+    }
+
+    @NSManaged public var article: String?
+    @NSManaged public var category: String?
+    @NSManaged public var title: String?
+
+}
